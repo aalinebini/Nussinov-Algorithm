03a036d523295c4dc3fdab9ad074e5f4811ed8fb
aalinebini <aalinerudbi@gmail.com>
2020-04-15 12:32:28 -0300

    Nussinov

--- Nussinov.py
+++ Nussinov.py
@@ -3,7 +3,6 @@
 import numpy as np
 
 # Lendo sequencia
-
 arq = open('/home/aline/Documentos/Teste.txt', 'r')
 seq = arq.read()
 seq = seq.strip()
@@ -12,57 +11,46 @@ print('Sequencia: {}'.format(seq))
 arq.close()
 
 # Criando matriz com zeros
-
 def Cria_Matriz(tamanho):
     return np.zeros([tamanho, tamanho], dtype=int)
 
-
 # Comparando para saber se as bases pareiam ou nao
-
 def Pareia(y, x):
+
     bases = (y, x)
     if bases in (('A', 'U'), ('U', 'A'), ('C', 'G'), ('G', 'C')):
         return 1
     return 0
 
-
 # Calculando campos para preenchimento da matriz
-
 def Calculo_matriz(M, i, j):
 
     # Condicoes de pareamento
     if (j < i) or (i == j) or (j == i + 1) or (j == i - 1):
         return 0
-
     else:
         k = i + 1
         list_k = []
         while (k < j):
             list_k.append(M[i, k] + M[k + 1, j])
             k = k + 1
-
         x0 = M[i + 1, j]
         x1 = M[i, j - 1]
         x2 = M[i + 1, j - 1] + (Pareia(seq[i], seq[j]))
         x3 = max(list_k)
     return max(x0, x1, x2, x3)
 
-
 # Preenchendo a matriz
-
 def Preenche_matriz(M, tam):
     for ck in range(2, tam):
         for ci in range(0, tam-ck):
            M[ci][ci+ck] = Calculo_matriz(M, ci, ci+ck)
     return M
 
-
 #Tracebacking
 
 #criando as pilhas
-
 def empilha(M):
-
     pilha1 = []
     pilha2 = []
     i = 0
@@ -72,7 +60,6 @@ def empilha(M):
     pilha1.append([i, j])
 
     while i < j:
-
         try:
             i, j = pilha1.pop()
         except IndexError:
@@ -88,7 +75,6 @@ def empilha(M):
             pilha2.append([i, j])
             pilha1.append([i+1, j-1])
 
-
         else:
             k = i + 1
             while k < j:
@@ -102,13 +88,14 @@ def empilha(M):
     return pilha2
 
 # Chamando os metodos
-
 Matriz = Cria_Matriz(len(seq))
 Matriz = Preenche_matriz(Matriz, len(seq))
 print(Matriz)
 print("Resultado: {}".format(empilha(Matriz)))
 
-###################################################################
+########################Exemplo########################
+
+'''
 Entrada:
 
 	Sequencia: ['G', 'G', 'G', 'A', 'A', 'A', 'U', 'C', 'C']
@@ -125,3 +112,4 @@ Saída:
 	 [0 0 0 0 0 0 0 0 0]]
 
 	Pares de bases: [[1, 8], [2, 7], [4, 6]]
+'''
